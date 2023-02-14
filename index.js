731bb58831dd8dc4f954da28da9e649ac2f1ffa5
barbaratimson <cokolek.sokol2014@mail.ru>
2023-02-14 05:39:32 +0000

    for axios

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors')
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors())
 
-app.get('/', (req, res) => res.json({message: "Hello, world!"}));
+app.post('/auth/',(req,res) =>{
+    const email = req.body.email    
+    const password = req.body.password
 
-app.listen(6060, () => console.log("Started."))
+    console.log('got request!', email, password)
+    if (email === 'nke@nake.ru' && password === '1234') {
+        res.json({
+            status: true,
+            token: `${email}-token`
+        })
+    } else {
+        res.json({
+            status: false
+        })
+    }
+})
+app.listen(5050, () => console.log('Started!'))
