80ceeadc19a37aa1734e9335676ba5d2138f6027
barbaratimson <cokolek.sokol2014@mail.ru>
2023-02-28 20:01:02 +0000

    chat

--- index.js
+++ index.js
@@ -2,70 +2,108 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 
 app.use(bodyParser.json());
 app.use(cors())
-let token = ''
 const users = [
 {email: "seregazloy@mail.ru",
 password: "baza2015",
 name: "Sergey",
-age: "40"},
+age: "40",
+token:""},
 {email: "tankist@yahoo.com",
 password: "sdfsf",
 name: "Volodya",
-age: "50"},
+age: "50",
+token:""},
 {email: "Sanya_CS@mail.ru",
 password: "Toxic",
 name: "Alexandr",
-age: "10"}
+age: "10",
+token:""}
 ]
 
-const auth = () =>{
-    return
-} 
-
-
-app.get('/me/',(req,res) => {
-    var auth = req.headers.authorization
-    auth = auth.split(' ')
-    if (auth[1] === token) {
-        auth = auth[1].substring(0, auth[1].length - 6)
+const authorizationHander = (req,res,next) =>{
+    const [type,token] = req.headers.authorization.split(' ')
+    const email = req.body.email 
+    if (type.toLowerCase() != 'bearer'){
         res.json({
-            auth
-        }) 
-    } else
+            message:"Bad token type"
+        })
+        return;
+    }
+    const user = users.find(
+        item => item.token === token
+    ) 
+    if (!users) {
+        res.json({
+            message:"Bad token"
+        })
+    } else {
+        req.locals = {
+            user
+        }
+        next()
+    }
+
+}
+
+app.get('/me/', authorizationHander,(req,res) => {
+    var user = req.locals.user
+    if (user){
     res.json({
-        message: "Bad token",
+        mail: user.email,
+        name: user.name
+    })
+}else {
+    res.json({
+        status:"User error"
+    })
+}
+})
+
+app.get('/users/', authorizationHander,(req,res) => {
+
+    res.json({
+       users
     })
 })
+
+
+app.post('/reg', (req,res) => {
+    const  username = req.headers.username
+    const  password = req.headers.password
+    const id =  users.length + 1
+     users.push({id,username:username,password:password,friends:[],token:""})
+     res.json({
+         message:"User created"
+     })
+     
+ })
 
 app.post('/auth/',(req,res) =>{
     const email = req.body.email    
     const password = req.body.password
-    let user = users.find(da => da.email == email)
-    if (!user) {
+    if (password && email){ 
+    let user = users.find(da => da.email == email && da.password === password)
+    if (user) {
+        let token = uuidv4();
+        user.token = token
         res.json({
-            message:"User not found"
+            status: true,
+            token: token,
         })
     } else {
-        let userId = users.indexOf(user)
-        let userMail = users[userId].email
-        let userPassword = users[userId].password
-        token = `${email}-token`
-        console.log('got request!', email, password)
-            if (email === userMail && password === userPassword) {
-                res.json({
-                status: true,
-                token: token
-             })
-    }   else {
-            res.json({
-                userId,
-                status: false
-            })
+        res.json({
+            status: false
+        })
     }
-    }
-    
+}else {
+    res.json({
+        status:"false",
+    })
+}
 })
+
 app.listen(5050, () => console.log('Started!'))
